cf4176487c28c09093b80d725181f14e317321e5
hugo <hugo.herrador@gmail.com>
2023-05-29 20:00:23 +0200

    Add easy lazy loading for the unique module of the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./books/books.module').then((b) => b.BooksModule),
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
